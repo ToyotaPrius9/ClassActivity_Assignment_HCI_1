7221f904ebf8047f45dadd38df3ca07333469e85
ToyotaPrius9 <114371673+ToyotaPrius9@users.noreply.github.com>
2022-12-17 12:22:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//Please put your answer here
+$(".signup-show").click(function(){
+    $("#signup-modal").show();
+  });
+
+$("#close-modal").click(function(){
+    $("#signup-modal").hide();
+  });
